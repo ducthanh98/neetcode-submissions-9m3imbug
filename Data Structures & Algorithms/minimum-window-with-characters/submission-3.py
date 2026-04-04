3ffd3065c3ab562ea37e15879c3f270e185dad60
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2026-04-04 12:03:06 +0700

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        state = {}
+        for v in t:
+            state[v] = state.get(v,0) + 1
+        count = len(t)
+        min_length = 999999
+        res = ""
+        l,r = 0,0
+        while(r < len(s)):
+            if(s[r] in state):
+                if(state[s[r]] > 0): 
+                    count -=1
+                state[s[r]] -= 1
+            while(count == 0):
+                if min_length > r - l + 1:
+                    min_length = r - l + 1
+                    res = s[l:r + 1]
+                
+                if(s[l] in state):
+                    state[s[l]] += 1                        
+                    if(state[s[l]] > 0):
+                        count += 1
+
+                l+= 1
+            r += 1
+
+
+        return res
+            
+            
