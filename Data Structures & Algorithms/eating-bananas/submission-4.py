1f3bb78dba70ef45ca2b78fdd695f00d24c86edd
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2026-04-09 16:59:22 +0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        res = float('inf')
+        piles.sort()
+
+        l,r = 1, piles[-1] + 1
+        while(l < r):
+            mid = (l +r) //2
+            times = sum([math.ceil(x / mid) for x in piles])
+
+            if times <= h:
+                if mid <= res:
+                    res = mid
+                
+                r = mid
+                
+            else:
+                l = mid + 1
+    
+
+
+        return res
